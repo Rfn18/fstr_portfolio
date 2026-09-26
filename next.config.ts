19e2b6f3af9c5324f7185ade1cd3083d33efe652
Fasterino <rinofaster89@gmail.com>
2026-09-26 11:24:05 +0700

    updated commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["framer-motion", "gsap"],
 };
 
 export default nextConfig;
